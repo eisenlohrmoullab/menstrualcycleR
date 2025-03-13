f815e628abb3a0558ae598099423dab88166009b
anagpa7 <anagpa7@uic.edu>
2025-03-13 17:00:25 -0500

    updating ui to have an option to download plot

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -11,8 +11,8 @@ ui <- fluidPage(
       selectInput("id_col", "Select ID Column:", choices = NULL),
       selectInput("date_col", "Select Date Column:", choices = NULL),
       selectInput("menses_col", "Select Menses Column:", choices = NULL),
-      selectInput("ovtoday_col", "Select Ovulation Column:", choices = NULL), 
-      hr(), 
+      selectInput("ovtoday_col", "Select Ovulation Column:", choices = NULL),
+      hr(),
       actionButton("process_data", "Process Data", class = "btn-success")
     ),
     
@@ -22,22 +22,25 @@ ui <- fluidPage(
         tabPanel("Data Availability", verbatimTextOutput("cycle_summary"), tableOutput("cycle_data")),
         tabPanel("Ovulation Analysis", tableOutput("ovulation_summary")),
         
-        # Symptom Data Tab
+        # Symptom Analysis Tab
         tabPanel("Symptom Data",
                  sidebarPanel(
-                   selectInput("symptom_col", "Select Symptom Column:", choices = NULL)
+                   selectInput("symptom_col_analysis", "Select Symptom Column for Analysis:", choices = NULL)
                  ),
                  mainPanel(
-                   tableOutput("symptom_check")
+                   tableOutput("symptom_analysis")
                  )
         ),
         
-        # Cycle Plot Tab
+        # Cycle Plot Tab with separate symptom selection
         tabPanel("Cycle Plot",
                  sidebarPanel(
+                   selectInput("symptom_col_plot", "Select Symptom Column for Plot:", choices = NULL),
                    selectInput("plot_centering", "Centering Phase:", choices = c("menses", "ovulation")),
                    checkboxInput("plot_impute", "Include Imputed Data", value = TRUE),
-                   actionButton("update_plot", "Update Plot", class = "btn-primary")
+                   actionButton("update_plot", "Update Plot", class = "btn-primary"),
+                   hr(),
+                   downloadButton("download_plot", "Download Cycle Plot")
                  ),
                  mainPanel(
                    plotOutput("cycle_plot")
