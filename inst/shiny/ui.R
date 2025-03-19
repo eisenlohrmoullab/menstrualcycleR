6ff8106a4df599fcd667c0fd6ce84c86776493b2
anagpa7 <anagpa7@uic.edu>
2025-03-19 15:20:02 -0500

    updating UI code to have appropriate tabs

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -1,5 +1,3 @@
-library(shiny)
-
 ui <- fluidPage(
   titlePanel("Phase Aligned Cycle Time Scaling (PACTS) with menstrualcycleR"),
   
@@ -9,7 +7,6 @@ ui <- fluidPage(
       actionButton("load_data", "Load Data", class = "btn-primary"),
       hr(),
       
-      # New cycle length bounds input
       numericInput("lower_bound", "Lower Bound of Cycle Length:", value = 21, min = 10, max = 40),
       numericInput("upper_bound", "Upper Bound of Cycle Length:", value = 35, min = 10, max = 40),
       
@@ -25,6 +22,26 @@ ui <- fluidPage(
       tabsetPanel(
         tabPanel("View Data", tableOutput("data_preview")),
         tabPanel("Ovulation Analysis", tableOutput("ovulation_summary")),
+        
+        tabPanel("Cycle Plot",
+                 sidebarPanel(
+                   selectInput("symptom_col_plot", "Select Symptom:", choices = NULL),
+                   selectInput("plot_centering", "Centering:", choices = c("menses", "ovulation")),
+                   checkboxInput("plot_impute", "Include Imputed Data", value = TRUE),
+                   actionButton("update_plot", "Update Plot"),
+                   downloadButton("download_plot", "Download Cycle Plot")
+                 ),
+                 mainPanel(plotOutput("cycle_plot"))
+        ),
+        
+        tabPanel("Individual Cycle Plot",
+                 sidebarPanel(
+                   selectInput("id_selected", "Select ID:", choices = NULL),
+                   checkboxGroupInput("symptom_cols_individual", "Select Symptoms:", choices = NULL)
+                 ),
+                 mainPanel(uiOutput("individual_cycle_plots"))
+        ),
+        
         tabPanel("Download", downloadButton("download_results", "Download Processed Data"))
       )
     )
