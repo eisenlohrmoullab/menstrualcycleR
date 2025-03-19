e85a33660f9fd641157f37689539ef9a631ae718
anagpa7 <anagpa7@uic.edu>
2025-03-19 16:17:52 -0500

    updating documentation for y-scale in the cycle_plot_individual() function

--- R/cycle_plot_individual.R
+++ R/cycle_plot_individual.R
@@ -7,7 +7,7 @@
 #' @param id Numeric id number for the specific individual for whom cycle plots should be generated.
 #' @param symptom A string specifying the symptom variable to analyze.
 #' @param centering A string indicating the centering phase of the cycle ("menses" or "ovulation").
-#' @param y_scale A string specifying the y-axis scale ("person-centered", "person-centered_roll", or "means").
+#' @param y_scale A string specifying the y-axis scale ("person-centered", "person-centered_roll", "raw", or "roll").
 #' @param include_impute A boolean indicating whether to use imputed cycle time values.
 #' @param rollingavg A numeric indicating how many days of a rolling average to use, the default is 5
 #'
