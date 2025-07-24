4299a5fc5b09d9b0bb0223e6fc8e7bac42f7de05
anagpa7 <anagpa7@uic.edu>
2025-07-24 16:23:44 -0500

    add description to launch_app function

--- R/launch_app.R
+++ R/launch_app.R
@@ -1,5 +1,13 @@
 #' Launch the Menstrual Cycle Shiny App
 #'
+#' This function launches an interactive Shiny application designed to help users upload and process their menstrual cycle data. 
+#' The app provides tools to apply Phase-Aligned Cycle Time Scaling (PACTS), generate scaled cycleday variables, and visualize results 
+#' in an intuitive, user-friendly interface.
+#'
+#' Users can upload a `.csv` file, process their data using built-in PACTS functionality, and explore cycle-aligned visualizations 
+#' to support analysis and interpretation.
+
+#'
 #' @export
 launch_app <- function() {
   appDir <- system.file("shiny", package = "menstrualcycleR")
