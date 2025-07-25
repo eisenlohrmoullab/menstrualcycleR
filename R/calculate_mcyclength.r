1612ac1a8c6d96f7ac22600bf2de19a4e94a9bb3
anagpa7 <anagpa7@uic.edu>
2025-07-25 11:57:35 -0500

    updating calcualte_mcyclength to not generate help file

--- R/calculate_mcyclength.r
+++ R/calculate_mcyclength.r
@@ -21,6 +21,7 @@
 #' - **`cyclenum`**: The number of complete menses-to-menses cycles for each individual (`id`).
 #' - **`cycle_incomplete`**: A binary variable (`0`/`1`), where `1` indicates that the row is not part of a complete cycle.
 #'
+#' @noRd
 #' 
 #' @examples 
 #' # Load the example dataset
