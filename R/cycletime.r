a4275177a295ef79e7b5ad78f68f7f13055b82d0
anagpa7 <anagpa7@uic.edu>
2025-08-12 10:40:40 -0500

    adding cyclic time to case_when statements at the end of cycle time helper function

--- R/cycletime.r
+++ R/cycletime.r
@@ -125,19 +125,42 @@ calculate_cycletime <- function(data, id, date, menses, ovtoday, lower_cyclength
       scaled_cycleday_imp_ov = dplyr::case_when(
         ovtoday | ovtoday_impute == 1 & is.na(scaled_cycleday_imp_ov) ~ 0,
         TRUE ~ scaled_cycleday_imp_ov
+      ), 
+      cyclic_time = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 1, 
+        TRUE ~ cyclic_time
+      ), 
+      cyclic_time_impute = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 1, 
+        TRUE ~ cyclic_time_impute
+      ), 
+      scaled_cycleday = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 1, 
+        TRUE ~ scaled_cycleday
+      ), 
+      scaled_cycleday_impute = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 1, 
+        TRUE ~ scaled_cycleday_impute
+      ), 
+      cyclic_time_ov = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 0, 
+        TRUE ~ cyclic_time_ov
+      ), 
+      cyclic_time_imp_ov = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 0, 
+        TRUE ~ cyclic_time_imp_ov
+      ), 
+      scaled_cycleday_ov = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 0, 
+        TRUE ~ scaled_cycleday_ov
+      ), 
+      scaled_cycleday_imp_ov = dplyr::case_when(
+        ovtoday | ovtoday_impute == 1 ~ 0, 
+        TRUE ~ scaled_cycleday_imp_ov
       )
     )
-  
-    data$cyclic_time = ifelse(ovtoday == 1|ovtoday_impute == 1, 1, cyclic_time)
-    data$cyclic_time_impute = ifelse(ovtoday == 1|ovtoday_impute == 1, 1, cyclic_time_impute)
-    data$scaled_cycleday = ifelse(ovtoday == 1|ovtoday_impute == 1, 1, scaled_cycleday)
-    data$scaled_cycleday_impute = ifelse(ovtoday == 1|ovtoday_impute == 1, 1, scaled_cycleday_impute)
-    data$cyclic_time_ov = ifelse(ovtoday == 1|ovtoday_impute == 1, 0, cyclic_time_ov)
-    data$cyclic_time_imp_ov = ifelse(ovtoday == 1|ovtoday_impute == 1, 0, cyclic_time_imp_ov)
-    data$scaled_cycleday_ov = ifelse(ovtoday == 1|ovtoday_impute == 1, 0, scaled_cycleday_ov)
-    data$scaled_cycleday_imp_ov = ifelse(ovtoday == 1|ovtoday_impute == 1, 0, scaled_cycleday_imp_ov)
     
-    data = data %>% arrange(id, date) %>% ungroup()
+    data = data %>% dplyr::arrange(id, date) %>% ungroup()
   #Select and return the relevant columns
   data <- data %>%
     dplyr::select(
